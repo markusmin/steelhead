a6e3729cabc4be9489561a2897a327f34b0d0230
Markus Min <markus.min@gmail.com>
2022-01-20 15:00:40 -0800

    began collating info on tributary arrays

--- R/03_fit_multistate.R
+++ R/03_fit_multistate.R
@@ -13,11 +13,29 @@ library(janitor)
 # Look at what sites
 JDR_site_count <- as.data.frame(table(JDR_det_hist$event_site_name))
 
+# Get unique
+JDR_event_site_metadata
+
 # Look at when fish were seen at what site
 # BON corner collector - looks like pretty much every year
 table(subset(JDR_det_hist, event_site_name == "BCC - BON PH2 Corner Collector")$start_time)
 
+# Dams in order
+dams <- c("Bonneville Adult Fishways (combined)", "McNary Adult Fishways (Combined)", 
+          "ICH - Ice Harbor Dam (Combined)", "PRA - Priest Rapids Adult",
+          "RIA - Rock Island Adult", "WEA - Wells Dam, DCPUD Adult Ladders",
+          "GRA - Lower Granite Dam Adult")
 
+# Natal tributary detection sites
+dam1_dam2_natal_sites <- JDR_event_site_metadata$event_site_name[grep("John Day", JDR_event_site_metadata$event_site_basin_name)]
+
+# Straying sites between BON and MCN
+dam1_dam2_stray_sites <- c("WHITEC - White Creek, Klickitat River Basin",
+                           "SHERFT - Sherars Falls Fishway Trap, Deschutes River",
+                           "TROU2C - Trout Creek, Deschutes River Watershed",
+                           )
+
+# Straying detection sites
 
 
 
