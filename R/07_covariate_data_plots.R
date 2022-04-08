56a988eb9628f4606bb9e957898d17a70d665329
Markus Min <markus.min@gmail.com>
2022-04-08 09:02:13 -0700

    added new script for exploratory plots of covariates

---
+++ R/07_covariate_data_plots.R
@@ -0,0 +1,81 @@
+# 07_covariate_data_plots
+
+# Exploratory plots of various covariates
+
+# Load libraries
+library(here)
+library(janitor)
+library(tidyverse)
+library(lubridate)
+library(ggthemes)
+
+temp_cov_df <- read.csv(here::here("covariate_data", "temperature_by_state.csv"), row.names = 1)
+spill_cov_df <- read.csv(here::here("covariate_data", "spill_by_state.csv"), row.names = 1)
+flow_cov_df <- read.csv(here::here("covariate_data", "flow_by_state.csv"), row.names = 1)
+
+# Create DF of state + dam
+dam_state <- data.frame(state = c("mouth.to.BON",
+                                  "MCN.to.ICH.or.PRA..ICH.",
+                                  "ICH.to.LGR",
+                                  "BON.to.MCN",           
+                                  "MCN.to.ICH.or.PRA..PRA.",
+                                  "PRA.to.RIS", 
+                                  "RIS.to.RRE", 
+                                  "RRE.to.WEL"),
+                        dam = c("BON",
+                                "ICH",
+                                "LGR",
+                                "MCN",
+                                "PRA",
+                                "RIS",
+                                "RRE",
+                                "WEL"))
+
+
+##### Temperature #####
+
+# Pivot longer
+temp_cov_df %>% 
+  pivot_longer(., cols = colnames(temp_cov_df[2:ncol(temp_cov_df)])) %>% 
+  dplyr::rename(state = name, temp = value) %>% 
+  mutate(date = ymd(date)) %>% 
+  left_join(., dam_state, by = "state")-> temp_long
+
+# Remove anomalously high values - above 25 C
+temp_long %>% 
+  subset(temp <= 25) -> temp_long
+
+ggplot(temp_long, aes(x = date, y = temp, color = dam)) +
+         geom_line() +
+  scale_color_tableau(palette = "Tableau 10")
+       
+
+##### Spill #####
+
+# Pivot longer
+spill_cov_df %>% 
+  pivot_longer(., cols = colnames(spill_cov_df[2:ncol(spill_cov_df)])) %>% 
+  dplyr::rename(state = name, spill = value) %>% 
+  mutate(date = ymd(date)) %>% 
+  left_join(., dam_state, by = "state") -> spill_long
+
+ggplot(spill_long, aes(x = date, y = spill, color = dam)) +
+  geom_line() +
+  scale_color_tableau(palette = "Tableau 10")
+
+
+##### Flow #####
+
+# Pivot longer
+flow_cov_df %>% 
+  pivot_longer(., cols = colnames(flow_cov_df[2:ncol(flow_cov_df)])) %>% 
+  dplyr::rename(state = name, flow = value) %>% 
+  mutate(date = ymd(date)) %>% 
+  left_join(., dam_state, by = "state") -> flow_long
+
+ggplot(flow_long, aes(x = date, y = flow, color = dam)) +
+  geom_line() +
+  scale_color_tableau(palette = "Tableau 10")
+
+
+
