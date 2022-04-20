f6036743433de37738244abd0b3c4ae6795c567e
Markus Min <markus.min@gmail.com>
2022-04-20 13:51:36 -0700

    updating code

--- R/09_fit_simulation.R
+++ R/09_fit_simulation.R
@@ -10,24 +10,26 @@ library(rjags)
 library(jagsUI)
 
 # Load data to fit model to
-sim_data <- readRDS(here::here("simulation", "sim_600.rds"))
-dates <- readRDS(here::here("simulation", "dates_600.rds"))
+sim_data <- readRDS(here::here("simulation", "sim_600_array.rds"))
+sim_data_list <- readRDS(here::here("simulation", "sim_600_list.rds"))
+
+dates <- as.matrix(readRDS(here::here("simulation", "dates_600_matrix.rds")))
+# Dates are numbers, with 2017-01-01 = 1
 
 # Read in covariates
-temp_sim_zscore_df <- read.csv(here::here("simulation", "temp_600.csv"))
-flow_sim_zscore_df <- read.csv(here::here("simulation", "flow_600.csv"))
-fish_sim_cat_data <- read.csv(here::here("simulation", "origin_rear_600.csv"))
+temp_sim <- as.matrix(read.csv(here::here("simulation", "temp_600.csv"), row.names = 1))
+flow_sim <- as.matrix(read.csv(here::here("simulation", "flow_600.csv"), row.names = 1))
+fish_sim_cat_data <- as.matrix(read.csv(here::here("simulation", "origin_rear_600.csv")))
 
 
 # Store quantities for loop
 # Store the total number of individuals
-n.ind <- length(sim_data)
+n.ind <- length(sim_data_list)
 
 # Store the number of observations per individual
-n.obs <- unlist(lapply(sim_data, ncol))
+n.obs <- unlist(lapply(sim_data_list, ncol))
 
 # Get number of possible movements from each site
-rownames(sim_data[[1]])
 possible_movements <- c("mainstem, mouth to BON" = 2,
                         "mainstem, BON to MCN" = 5,
                         "mainstem, MCN to ICH or PRA" = 5,
@@ -43,7 +45,7 @@ possible_movements <- c(2,5,5,2,3,2,2,2,2,0)
 
 # Create lists for covariates, to inform which covariates to choose for each transition
 cov_index_list <- list()
-for (i in 1:length(possible_movements)){
+for (i in 1:(length(possible_movements)-1)){
   vec <- vector(length = (possible_movements[i]-1))
   
   # Store in list
@@ -69,6 +71,18 @@ cov_index_list[[8]] <- c(7) # YAK
 # Tucannon
 cov_index_list[[9]] <- c(8) # TUC
 
+# Turn this into a matrix for JAGS
+cov_index_mat <- matrix(data = c(1, NA, NA, NA,
+                                  1, 2, 6, 5,
+                                  2, 3, 4, 7,
+                                  3, NA, NA, NA,
+                                  4, 8, NA, NA,
+                                  6, NA, NA, NA,
+                                  5, NA, NA, NA,
+                                  7, NA, NA, NA,
+                                  8, NA, NA, NA), 
+                        ncol = 4, nrow = 9, byrow = TRUE)
+
 # Get the state that each fish was in at each n.obs
 
 # First initialize an empty list
@@ -85,12 +99,130 @@ for (i in 1:n.ind){
 for (i in 1:n.ind){
   for (j in 1:(n.obs[i]-1)){
     # states_list[[i]][j] <- rownames(as.data.frame(which(sim_data[[i]][,j] == 1)))
-    states_list[[i]][j] <- which(sim_data[[i]][,j] == 1) # Get the index of the site instead of the name
+    states_list[[i]][j] <- which(sim_data[,j,i] == 1) # Get the index of the site instead of the name
   }
 }
 
+# Turn into matrix for JAGS
+states_mat <- matrix(nrow = n.ind, ncol = max(n.obs))
+for (i in 1:n.ind){
+  states_mat[i,1:(n.obs[i]-1)] <- states_list[[i]]
+}
+
+
+
 # Get the beta vectors - will be different for each state
-# NOTE: every single one of these will require priors
+
+# # Mainstem, mouth to BON
+# B_vec_1 <- c(b0_MB_BM, bflow_MB_BM, btemp_MB_BM, brear_MB_BM[rear], borigin_MB_BM[origin])
+# 
+# # Mainstem, BON to MCN
+# B_vec_2 <- c(b0_BM_MB, b0_BM_MIP,  b0_BM_DES,  b0_BM_JDR,
+#              bflow_BM_MB, bflow_BM_MIP,  bflow_BM_DES,  bflow_BM_JDR,
+#              btemp_BM_MB, btemp_BM_MIP,  btemp_BM_DES,  btemp_BM_JDR,
+#              brear_BM_MB[rear], brear_BM_MIP[rear],  brear_BM_DES[rear],  brear_BM_JDR[rear],
+#              borigin_BM_MB[origin], borigin_BM_MIP[origin],  borigin_BM_DES[origin], borigin_BM_JDR[origin])
+# 
+# # Mainstem, MCN to ICH or PRA
+# B_vec_3 <- c(b0_MIP_BM, b0_MIP_PR, b0_MIP_IL, b0_MIP_YAK,
+#              bflow_MIP_BM, bflow_MIP_PR, bflow_MIP_IL, bflow_MIP_YAK,
+#              btemp_MIP_BM, btemp_MIP_PR, btemp_MIP_IL, btemp_MIP_YAK,
+#              brear_MIP_BM[rear], brear_MIP_PR[rear], brear_MIP_IL[rear], brear_MIP_YAK[rear],
+#              borigin_MIP_BM[origin], borigin_MIP_PR[origin], borigin_MIP_IL[origin], borigin_MIP_YAK[origin])
+# 
+# # Mainstem, PRA to RIS
+# B_vec_4 <- c(b0_PR_MIP, bflow_PR_MIP, btemp_PR_MIP, brear_PR_MIP[rear], borigin_PR_MIP[origin])
+# 
+# # Mainstem, ICH to LGR
+# B_vec_5 <- c(b0_IL_MIP, b0_IL_TUC, 
+#              bflow_IL_MIP, bflow_IL_TUC, 
+#              btemp_IL_MIP, btemp_IL_TUC, 
+#              brear_IL_MIP[rear], brear_IL_TUC[rear], 
+#              borigin_IL_MIP[origin], borigin_IL_TUC[origin])
+# 
+# # Deschutes River
+# B_vec_6 <- c(b0_DES_BM, bflow_DES_BM, btemp_DES_BM, brear_DES_BM[rear], borigin_DES_BM[origin])
+# 
+# # John Day River
+# B_vec_7 <- c(b0_JDR_BM, bflow_JDR_BM, btemp_JDR_BM, brear_JDR_BM[rear], borigin_JDR_BM[origin])
+# 
+# # Yakima River
+# B_vec_8 <- c(b0_YAK_MIP, bflow_YAK_MIP, btemp_YAK_MIP, brear_YAK_MIP[rear], borigin_YAK_MIP[origin])
+# 
+# # Tucannon River
+# B_vec_9 <- c(b0_TUC_IL, bflow_TUC_IL, btemp_TUC_IL, brear_TUC_IL[rear], borigin_TUC_IL[origin])
+# 
+# # Store beta vectors in a list
+# B_vec_list <- list(B_vec_1, B_vec_2, B_vec_3, B_vec_4, B_vec_5, 
+#                    B_vec_6, B_vec_7, B_vec_8, B_vec_9)
+
+# Fit the model in JAGS
+
+cat("
+model {
+
+#state-space likelihood 
+for(i in 1:n.ind){ # Loop through the detection matrices for each individual
+  for(j in 1:(n.obs[i]-1)){ # Loop through each of the observations, stopping at the loss column (-1)
+  
+    # Get the rear type
+    rear <- fish_sim_cat_data[i,3]
+    
+    # Get the origin
+    origin <- fish_sim_cat_data[i,2]
+    
+    
+    # Get the current state
+    cur_state <- states_mat[i,j]
+    
+    # Get number of possible movements
+    n_movements <- possible_movements[cur_state]
+    
+    # index the covariate data
+    temps <- as.numeric(temp_sim[dates[i,j],])
+    flows <- as.numeric(flow_sim[dates[i,j],])
+      
+    
+    # Get the temperature values for this state
+    temp <- vector(length = (n_movements - 1))
+    for (m in 1:(n_movements-1)){
+      temp[m] <- temps[cov_index_mat[cur_state,m]]
+    }
+    
+    
+    # Get the flow values for this state
+    flow <- vector(length = (n_movements - 1))
+    for (m in 1:(n_movements-1)){
+      flow[m] <- flows[temps[cov_index_mat[cur_state,m]]]
+    }
+    
+    
+    # Create a design matrix for number of possible movements
+    X <- matrix(0, nrow = (n_movements - 1), ncol = (n_movements - 1) * 5)
+    
+    # Populate the matrix
+    for (m in 1:(n_movements-1)){
+      for (k in 1:5){
+        X[m, (k-1)*(n_movements-1) + m] <- 1
+      }
+      
+    }
+    
+    # Populate the temperature and flow elements of the matrix with the covariate values
+    
+    # Temperature
+    for (m in 1:(n_movements-1)){
+        X[m, m + (n_movements-1)] <- temp[m]
+    }
+      
+    # Flow
+    for (m in 1:(n_movements-1)){
+        X[m, m + (n_movements-1)*2] <- flow[m]
+    }
+      
+    # Multiply design matrix by beta vector to get movement probabilities
+    
+    # Get the beta vectors - will be different for each state
 
 # Mainstem, mouth to BON
 B_vec_1 <- c(b0_MB_BM, bflow_MB_BM, btemp_MB_BM, brear_MB_BM[rear], borigin_MB_BM[origin])
@@ -131,74 +263,9 @@ B_vec_8 <- c(b0_YAK_MIP, bflow_YAK_MIP, btemp_YAK_MIP, brear_YAK_MIP[rear], bori
 # Tucannon River
 B_vec_9 <- c(b0_TUC_IL, bflow_TUC_IL, btemp_TUC_IL, brear_TUC_IL[rear], borigin_TUC_IL[origin])
 
+# Store beta vectors in a list
 B_vec_list <- list(B_vec_1, B_vec_2, B_vec_3, B_vec_4, B_vec_5, 
                    B_vec_6, B_vec_7, B_vec_8, B_vec_9)
-
-# Fit the model in JAGS
-
-cat("
-model {
-
-#state-space likelihood 
-for(i in 1:n.ind){ # Loop through the detection matrices for each individual
-  for(j in 1:(n.obs[i]-1)){ # Loop through each of the observations, stopping at the loss column (-1)
-  
-    # Get the rear type
-    rear <- fish_sim_cat_data[i,3]
-    
-    # Get the origin
-    origin <- fish_sim_cat_data[i,2]
-    
-    
-    # Get the current state
-    cur_state <- states_list[[i]][j]
-    
-    # Get number of possible movements
-    n_movements <- possible_movements[cur_state]
-    
-    # index the covariate data
-    temps <- as.numeric(temp_sim_zscore_df[temp_sim_zscore_df$date == dates[[i]][j],][2:ncol(temp_sim_zscore_df)])
-    flows <- as.numeric(flow_sim_zscore_df[flow_sim_zscore_df$date == dates[[i]][j],][2:ncol(flow_sim_zscore_df)])
-      
-    
-    # Get the temperature values for this state
-    temp <- vector(length = (n_movements - 1))
-    for (m in 1:(n_movements-1)){
-      temp[m] <- temps[cov_index_list[[cur_state]][m]]
-    }
-    
-    
-    # Get the flow values for this state
-    flow <- vector(length = (n_movements - 1))
-    for (m in 1:(n_movements-1)){
-      flow[m] <- flows[cov_index_list[[cur_state]][m]]
-    }
-    
-    
-    # Create a design matrix for number of possible movements
-    X <- matrix(0, nrow = (n_movements - 1), ncol = (n_movements - 1) * 5)
-    
-    # Populate the matrix
-    for (m in 1:(n_movements-1)){
-      for (k in 1:5){
-        X[m, (k-1)*(n_movements-1) + m] <- 1
-      }
-      
-    }
-    
-    # Populate the temperature and flow elements of the matrix with the covariate values
-    
-    # Temperature
-    for (m in 1:(n_movements-1)){
-        X[m, m + (n_movements-1)] <- temp[m]
-    }
-      
-    # Flow
-    for (m in 1:(n_movements-1)){
-        X[m, m + (n_movements-1)*2] <- flow[m]
-    }
-      
-    # Multiply design matrix by beta vector to get movement probabilities
     
     p <- X %*% B_vec_list[[cur_state]]
     
@@ -377,9 +444,9 @@ for(i in 1:n.ind){ # Loop through the detection matrices for each individual
 
 # Get data
 # Bundle data
-data <- list(y = sim_states, fish_sim_cat_data = fish_sim_cat_data, temp_sim_zscore_df = temp_sim_zscore_df, 
-             n.ind = n.ind, n.obs = n.obs, possible_movements = possible_movements, B_vec_list = B_vec_list,
-             flow_sim_zscore_df = flow_sim_zscore_df, states_list = states_list, cov_index_list = cov_index_list)
+data <- list(y = sim_data, fish_sim_cat_data = fish_sim_cat_data, temp_sim = temp_sim,
+             n.ind = n.ind, n.obs = n.obs, possible_movements = possible_movements,
+             flow_sim = flow_sim, states_mat = states_mat, cov_index_mat = cov_index_mat)
 
 # Initial values
 inits <- function(){list(
@@ -438,7 +505,7 @@ inits <- function(){list(
                              b0_PR_MIP = runif(1, -1, 1),
                              bflow_PR_MIP = runif(1, -1, 1),
                              btemp_PR_MIP = runif(1, -1, 1),
-                               brear_PR_MIP[i] = runif(2, -1, 1),
+                               brear_PR_MIP = runif(2, -1, 1),
                                borigin_PR_MIP = runif(3, -1, 1),
                              
                              # Mainstem, ICH to LGR
@@ -479,7 +546,7 @@ inits <- function(){list(
                              bflow_TUC_IL = runif(1, -1, 1),
                              btemp_TUC_IL = runif(1, -1, 1),
                               brear_TUC_IL = runif(2, -1, 1),
-                              borigin_TUC_IL = runif(3, -1, 1),
+                              borigin_TUC_IL = runif(3, -1, 1)
                              )}  
 
 # Parameters monitored
