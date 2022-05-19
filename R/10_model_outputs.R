b33243129078bc75cc85babea024c6d665da0dcc
Markus Min <markus.min@gmail.com>
2022-05-19 13:05:27 -0700

    plotted temp + flow

--- R/10_model_outputs.R
+++ R/10_model_outputs.R
@@ -429,7 +429,183 @@ ggsave(here::here("simulation", "figures", "sim600_temp_plots_btemp_bad.png"), h
 
 
 
+
+##### Plot simulation results - temperature + flow model #####
+# Here, remove all of the tributary temperature effects - there is no temp data for the tributaries
+params_in_temp_flow_model <- c("b0_matrix[2,1]",
+                     "b0_matrix[1,2]",
+                     "b0_matrix[3,2]",
+                     "b0_matrix[6,2]",
+                     "b0_matrix[7,2]",
+                     "b0_matrix[2,3]",
+                     "b0_matrix[4,3]",
+                     "b0_matrix[5,3]",
+                     "b0_matrix[9,3]",
+                     "b0_matrix[3,4]",
+                     "b0_matrix[3,5]",
+                     "b0_matrix[8,5]",
+                     "b0_matrix[2,6]",
+                     "b0_matrix[2,7]",
+                     "b0_matrix[5,8]",
+                     "b0_matrix[3,9]",
+                     "btemp_matrix[2,1]",
+                     "btemp_matrix[1,2]",
+                     "btemp_matrix[3,2]",
+                     # "btemp_matrix[6,2]",
+                     # "btemp_matrix[7,2]",
+                     "btemp_matrix[2,3]",
+                     "btemp_matrix[4,3]",
+                     "btemp_matrix[5,3]",
+                     # "btemp_matrix[9,3]",
+                     "btemp_matrix[3,4]",
+                     "btemp_matrix[3,5]",
+                     # "btemp_matrix[8,5]",
+                     "btemp_matrix[2,6]",
+                     "btemp_matrix[2,7]",
+                     "btemp_matrix[5,8]",
+                     "btemp_matrix[3,9]",
+                     "bflow_matrix[2,1]",
+                     "bflow_matrix[1,2]",
+                     "bflow_matrix[3,2]",
+                     # "bflow_matrix[6,2]",
+                     # "bflow_matrix[7,2]",
+                     "bflow_matrix[2,3]",
+                     "bflow_matrix[4,3]",
+                     "bflow_matrix[5,3]",
+                     # "bflow_matrix[9,3]",
+                     "bflow_matrix[3,4]",
+                     "bflow_matrix[3,5]",
+                     # "bflow_matrix[8,5]",
+                     "bflow_matrix[2,6]",
+                     "bflow_matrix[2,7]",
+                     "bflow_matrix[5,8]",
+                     "bflow_matrix[3,9]")
+
+# Return 2 facet-wrapped plots, one for each beta matrix
+# The hlines used as referernce will have to be changed for each
+simulation_plots_temp_flow <- function(JAGS_list, params_in_model){
+  JAGS_runs_comp_b0 <- data.frame("parameter" = NA, "mean" = NA, "q2.5" = NA, "q97.5" = NA)
+  JAGS_runs_comp_btemp <- data.frame("parameter" = NA, "mean" = NA, "q2.5" = NA, "q97.5" = NA)
+  JAGS_runs_comp_bflow <- data.frame("parameter" = NA, "mean" = NA, "q2.5" = NA, "q97.5" = NA)
+  for (i in 1:length(JAGS_list)){
+    as.data.frame(JAGS_list[[i]]$BUGSoutput$summary) %>% 
+      rownames_to_column("parameter") %>% 
+      subset(parameter %in% params_in_model) %>% 
+      dplyr::rename(q2.5 = `2.5%`, q97.5 = `97.5%`) %>% 
+      dplyr::select(parameter, mean, q2.5, q97.5) %>% 
+      mutate(run = paste0(i)) -> summary
+    
+    # Change run to a factor for plotting
+    summary %>% 
+      mutate(run = factor(run, levels = c("1", "2", "3", "4", "5", "6", "7", "8", "9", "10"))) -> summary
+    
+    # Split the summary into five, one for each parameter
+    summary %>% 
+      filter(grepl("b0", parameter)) -> b0_summary
+    summary %>% 
+      filter(grepl("btemp", parameter))  -> btemp_summary
+    summary %>% 
+      filter(grepl("bflow", parameter))  -> bflow_summary
+    
+    JAGS_runs_comp_b0 %>% 
+      bind_rows(., b0_summary) -> JAGS_runs_comp_b0
+    
+    JAGS_runs_comp_btemp %>% 
+      bind_rows(., btemp_summary) -> JAGS_runs_comp_btemp
+    
+    JAGS_runs_comp_bflow %>% 
+      bind_rows(., bflow_summary) -> JAGS_runs_comp_bflow
+  }
+  
+  
+  # Create the plot
+  JAGS_runs_comp_b0 <- subset(JAGS_runs_comp_b0, !(is.na(parameter))) 
+  JAGS_runs_comp_btemp <- subset(JAGS_runs_comp_btemp, !(is.na(parameter))) 
+  JAGS_runs_comp_bflow <- subset(JAGS_runs_comp_bflow, !(is.na(parameter))) 
+  
+  b0_plot <- ggplot(JAGS_runs_comp_b0, aes(y = mean, x = run)) +
+    geom_point() +
+    geom_linerange(aes(ymin = q2.5, ymax = q97.5)) +
+    geom_hline(yintercept = 1, lty = 2) +
+    facet_wrap(~parameter) +
+    theme(axis.text = element_text(size = 12),
+          axis.title = element_text(size = 15),
+          strip.text.x = element_text(size = 12)) +
+    ylab("Estimate") +
+    scale_y_continuous(breaks = c(0.5, 1, 1.5), limits = c(0,2)) +
+    xlab("Run")
+  
+  btemp_plot <- ggplot(JAGS_runs_comp_btemp, aes(y = mean, x = run)) +
+    geom_point() +
+    geom_linerange(aes(ymin = q2.5, ymax = q97.5)) +
+    # geom_hline(yintercept = 1, lty = 2) +
+    facet_wrap(~parameter) +
+    theme(axis.text = element_text(size = 12),
+          axis.title = element_text(size = 15),
+          strip.text.x = element_text(size = 12)) +
+    ylab("Estimate") +
+    scale_y_continuous(breaks = c(-1, 0, 1), limits = c(-2, 2)) +
+    xlab("Run")
+  
+  bflow_plot <- ggplot(JAGS_runs_comp_bflow, aes(y = mean, x = run)) +
+    geom_point() +
+    geom_linerange(aes(ymin = q2.5, ymax = q97.5)) +
+    # geom_hline(yintercept = 1, lty = 2) +
+    facet_wrap(~parameter) +
+    theme(axis.text = element_text(size = 12),
+          axis.title = element_text(size = 15),
+          strip.text.x = element_text(size = 12)) +
+    ylab("Estimate") +
+    scale_y_continuous(breaks = c(-1, 0, 1), limits = c(-2, 2)) +
+    xlab("Run")
+  
+  # Annotations
+  actual_values_temp <- data.frame(parameter = c("btemp_matrix[1,2]",
+                                            "btemp_matrix[2,1]",
+                                            "btemp_matrix[2,3]",
+                                            "btemp_matrix[2,6]",
+                                            "btemp_matrix[2,7]",
+                                            "btemp_matrix[3,2]",
+                                            "btemp_matrix[3,4]",
+                                            "btemp_matrix[3,5]",
+                                            "btemp_matrix[3,9]",
+                                            "btemp_matrix[4,3]",
+                                            "btemp_matrix[5,3]",
+                                            "btemp_matrix[5,8]"
+  ),
+  yint = c(0,0,0.5,0,0,-0.5,0.3,0,0,0,0.2,0))
+  
+  actual_values_flow <- data.frame(parameter = c("bflow_matrix[1,2]",
+                                                 "bflow_matrix[2,1]",
+                                                 "bflow_matrix[2,3]",
+                                                 "bflow_matrix[2,6]",
+                                                 "bflow_matrix[2,7]",
+                                                 "bflow_matrix[3,2]",
+                                                 "bflow_matrix[3,4]",
+                                                 "bflow_matrix[3,5]",
+                                                 "bflow_matrix[3,9]",
+                                                 "bflow_matrix[4,3]",
+                                                 "bflow_matrix[5,3]",
+                                                 "bflow_matrix[5,8]"
+  ),
+  yint = c(0,0.5,0,0,0,0.05,0,-0.3,0,0,0,0))
+  
+  btemp_plot +
+    geom_hline(data = actual_values_temp, aes(yintercept = yint), lty = 2) -> btemp_plot
+  
+  bflow_plot +
+    geom_hline(data = actual_values_flow, aes(yintercept = yint), lty = 2) -> bflow_plot
+  
+  return(list(b0_plot, btemp_plot, bflow_plot))
+}
+
+
+
+
+
+
 ##### Plot simulation results - ORIGIN + REAR model #####
+
 params_in_origin_rear_model <- c("b0_matrix[2,1]",
                      "b0_matrix[1,2]",
                      "b0_matrix[3,2]",
@@ -771,7 +947,7 @@ simulation_plots_origin <- function(JAGS_list, params_in_model = origin_params){
           axis.title = element_text(size = 15),
           strip.text.x = element_text(size = 12)) +
     ylab("Estimate") +
-    # scale_y_continuous(breaks = c(0.5, 1, 1.5), limits = c(0,2)) +
+    scale_y_continuous(breaks = c(0.5, 1, 1.5), limits = c(0,2)) +
     xlab("Run")
   
   borigin1_plot <- ggplot(JAGS_runs_comp_borigin1, aes(y = mean, x = run)) +
@@ -783,7 +959,7 @@ simulation_plots_origin <- function(JAGS_list, params_in_model = origin_params){
           axis.title = element_text(size = 15),
           strip.text.x = element_text(size = 12)) +
     ylab("Estimate") +
-    # scale_y_continuous(breaks = c(0.5, 1, 1.5), limits = c(0,2)) +
+    scale_y_continuous(breaks = c(-1, 0 ,1), limits = c(-2,2)) +
     xlab("Run")
   
   borigin2_plot <- ggplot(JAGS_runs_comp_borigin2, aes(y = mean, x = run)) +
@@ -795,10 +971,12 @@ simulation_plots_origin <- function(JAGS_list, params_in_model = origin_params){
           axis.title = element_text(size = 15),
           strip.text.x = element_text(size = 12)) +
     ylab("Estimate") +
-    # scale_y_continuous(breaks = c(0.5, 1, 1.5), limits = c(0,2)) +
+    scale_y_continuous(breaks = c(-1, 0 ,1), limits = c(-2,2)) +
     xlab("Run")
   
   # Annotations
+  
+  ### start old values
   borigin1_values <- data.frame(parameter = c("borigin1_matrix[2,1]",
                                               "borigin1_matrix[1,2]",
                                               "borigin1_matrix[3,2]",
@@ -837,15 +1015,57 @@ simulation_plots_origin <- function(JAGS_list, params_in_model = origin_params){
   ),
   yint = c(0, 0, 0, 0, 0, 0, 0, 0.5, -1, 0, -0.5, 0.5, 0, 0, -0.5, 1))
   
+  ### end old values
+  
+  
+  actual_values_borigin1 <- data.frame(parameter = c("borigin1_matrix[1,2]",
+                                                     "borigin1_matrix[2,1]",
+                                                     "borigin1_matrix[2,3]",
+                                                     "borigin1_matrix[2,6]",
+                                                     "borigin1_matrix[2,7]",
+                                                     "borigin1_matrix[3,2]",
+                                                     "borigin1_matrix[3,4]",
+                                                     "borigin1_matrix[3,5]",
+                                                     "borigin1_matrix[3,9]",
+                                                     "borigin1_matrix[4,3]",
+                                                     "borigin1_matrix[5,3]",
+                                                     "borigin1_matrix[5,8]",
+                                                     "borigin1_matrix[6,2]",
+                                                     "borigin1_matrix[7,2]",
+                                                     "borigin1_matrix[8,5]",
+                                                     "borigin1_matrix[9,3]"
+  ),
+  yint = c(0,0,-0.5,0.25,0.5,0.5,0,-0.5,-0.25,0,0.25,-0.25,0,-0.5,0.25,0.25))
+  
+  actual_values_borigin2 <- data.frame(parameter = c("borigin2_matrix[1,2]",
+                                                     "borigin2_matrix[2,1]",
+                                                     "borigin2_matrix[2,3]",
+                                                     "borigin2_matrix[2,6]",
+                                                     "borigin2_matrix[2,7]",
+                                                     "borigin2_matrix[3,2]",
+                                                     "borigin2_matrix[3,4]",
+                                                     "borigin2_matrix[3,5]",
+                                                     "borigin2_matrix[3,9]",
+                                                     "borigin2_matrix[4,3]",
+                                                     "borigin2_matrix[5,3]",
+                                                     "borigin2_matrix[5,8]",
+                                                     "borigin2_matrix[6,2]",
+                                                     "borigin2_matrix[7,2]",
+                                                     "borigin2_matrix[8,5]",
+                                                     "borigin2_matrix[9,3]"
+  ),
+  yint = c(0,0,0.25,-0.125,-0.25,-0.25,0,0,0.5,0,0.25,-0.25,0,0.25,0.25,-0.5))
+  
   borigin1_plot +
-    geom_hline(data = borigin1_values, aes(yintercept = yint), lty = 2) -> borigin1_plot
+    geom_hline(data = actual_values_borigin1, aes(yintercept = yint), lty = 2) -> borigin1_plot
   
   borigin2_plot +
-    geom_hline(data = borigin2_values, aes(yintercept = yint), lty = 2) -> borigin2_plot
+    geom_hline(data = actual_values_borigin2, aes(yintercept = yint), lty = 2) -> borigin2_plot
   
   
   return(list(b0_plot, borigin1_plot, borigin2_plot))
 }
+
 
 ##### 600 fish, origin only ######
 
@@ -867,21 +1087,30 @@ ggsave(here::here("simulation", "figures", "sim600_origin_plots_borigin2.png"), 
 
 ##### 1200 fish, origin only ######
 
-JAGS_1200_origin_list <- readRDS( here::here("simulation", "JAGS_cov_origin_1200_list.rds"))
-JAGS_obj <- JAGS_1200_origin_list[[3]]
+origin_1200_1 <- readRDS(here::here("from_hyak_transfer", "2022-05-17_origin_1200", "JAGS_cov_origin_1200_list_1.rds"))
+origin_1200_2 <- readRDS(here::here("from_hyak_transfer", "2022-05-17_origin_1200", "JAGS_cov_origin_1200_list_2.rds"))
+origin_1200_3 <- readRDS(here::here("from_hyak_transfer", "2022-05-17_origin_1200", "JAGS_cov_origin_1200_list_3.rds"))
+origin_1200_4 <- readRDS(here::here("from_hyak_transfer", "2022-05-17_origin_1200", "JAGS_cov_origin_1200_list_4.rds"))
+origin_1200_5 <- readRDS(here::here("from_hyak_transfer", "2022-05-17_origin_1200", "JAGS_cov_origin_1200_list_5.rds"))
+
+# Combine these into a single list
+JAGS_1200_origin_list <- c(list(origin_1200_1[[1]]), list(origin_1200_1[[2]]), list(origin_1200_2[[3]]), list(origin_1200_2[[4]]),
+                    list(origin_1200_3[[5]]), list(origin_1200_3[[6]]), list(origin_1200_4[[7]]), list(origin_1200_4[[8]]),
+                    list(origin_1200_5[[9]]), list(origin_1200_5[[10]]))
+
+# JAGS_1200_origin_list <- readRDS(here::here("from_hyak_transfer", "2022-05-17_origin_1200", "JAGS_cov_origin_1200_list.rds"))
+# JAGS_obj <- JAGS_1200_origin_list[[3]]
 mod_mcmc <- as.mcmc(JAGS_obj)
-summary(mod_mcmc)
-plot(mod_mcmc)
+# summary(mod_mcmc)
+# plot(mod_mcmc)
 
-
-JAGS_1200_origin_list <- readRDS( here::here("simulation", "JAGS_cov_origin_1200_list.rds"))
 sim1200_origin_plots <- simulation_plots_origin(JAGS_list = JAGS_1200_origin_list)
-sim1200_origin_plots[[1]]
-sim1200_origin_plots[[2]]
-sim1200_origin_plots[[3]]
-ggsave(here::here("simulation", "figures", "sim1200_origin_plots_b0.png"), height = 6, width = 10, sim1200_origin_plots[[1]])
-ggsave(here::here("simulation", "figures", "sim1200_origin_plots_borigin1.png"), height = 6, width = 10, sim1200_origin_plots[[2]])
-ggsave(here::here("simulation", "figures", "sim1200_origin_plots_borigin2.png"), height = 6, width = 10, sim1200_origin_plots[[3]])
+# sim1200_origin_plots[[1]]
+# sim1200_origin_plots[[2]]
+# sim1200_origin_plots[[3]]
+ggsave(here::here("simulation", "figures", "origin_only", "sim1200_origin_plots_b0.png"), height = 6, width = 10, sim1200_origin_plots[[1]])
+ggsave(here::here("simulation", "figures", "origin_only", "sim1200_origin_plots_borigin1.png"), height = 6, width = 10, sim1200_origin_plots[[2]])
+ggsave(here::here("simulation", "figures", "origin_only", "sim1200_origin_plots_borigin2.png"), height = 6, width = 10, sim1200_origin_plots[[3]])
 
 
 ##### 1200 fish, temperature and flow ######
@@ -993,6 +1222,34 @@ sim1200_temp_plots <- simulation_plots_temp(JAGS_list = temp_1200, params_in_mod
 # sim600_temp_plots[[2]]
 ggsave(here::here("simulation", "figures", "temperature_only", "sim1200_temp_plots_b0.png"), height = 6, width = 10, sim1200_temp_plots[[1]])
 ggsave(here::here("simulation", "figures", "temperature_only", "sim1200_temp_plots_btemp.png"), height = 6, width = 10, sim1200_temp_plots[[2]])
+
+
+##### 1200 fish, temperature + flow only #####
+
+
+temp_flow_1200_1 <- readRDS(here::here("from_hyak_transfer", "2022-05-18_temp_flow_1200", "JAGS_temp_flow_1200_list_1.rds"))
+temp_flow_1200_2 <- readRDS(here::here("from_hyak_transfer", "2022-05-18_temp_flow_1200", "JAGS_temp_flow_1200_list_2.rds"))
+temp_flow_1200_3 <- readRDS(here::here("from_hyak_transfer", "2022-05-18_temp_flow_1200", "JAGS_temp_flow_1200_list_3.rds"))
+temp_flow_1200_4 <- readRDS(here::here("from_hyak_transfer", "2022-05-18_temp_flow_1200", "JAGS_temp_flow_1200_list_4.rds"))
+temp_flow_1200_5 <- readRDS(here::here("from_hyak_transfer", "2022-05-18_temp_flow_1200", "JAGS_temp_flow_1200_list_5.rds"))
+
+# Combine these into a single list
+temp_flow_1200 <- c(list(temp_flow_1200_1[[1]]), list(temp_flow_1200_1[[2]]), list(temp_flow_1200_2[[3]]), list(temp_flow_1200_2[[4]]),
+               list(temp_flow_1200_3[[5]]), list(temp_flow_1200_3[[6]]), list(temp_flow_1200_4[[7]]), list(temp_flow_1200_4[[8]]),
+               list(temp_flow_1200_5[[9]]), list(temp_flow_1200_5[[10]]))
+
+# Inspect chains for one run
+JAGS_obj <- temp_flow_1200[[1]]
+mod_mcmc <- as.mcmc(JAGS_obj)
+# plot(mod_mcmc)
+
+# Use plotting function
+sim1200_temp_flow_plots <- simulation_plots_temp_flow(JAGS_list = temp_flow_1200, params_in_model = params_in_temp_flow_model)
+# sim600_temp_flow_plots[[1]]
+# sim600_temp_flow_plots[[2]]
+ggsave(here::here("simulation", "figures", "temperature_flow", "sim1200_temp_flow_plots_b0.png"), height = 6, width = 10, sim1200_temp_flow_plots[[1]])
+ggsave(here::here("simulation", "figures", "temperature_flow", "sim1200_temp_flow_plots_btemp.png"), height = 6, width = 10, sim1200_temp_flow_plots[[2]])
+ggsave(here::here("simulation", "figures", "temperature_flow", "sim1200_temp_flow_plots_bflow.png"), height = 6, width = 10, sim1200_temp_flow_plots[[3]])
 
 
 
