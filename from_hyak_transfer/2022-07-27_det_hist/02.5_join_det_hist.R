3ad5a2d4bee9ace7a5031431b6bc2acf81bdd9f5
Markus Min <markus.min@gmail.com>
2022-08-04 15:57:33 -0700

    making post-hoc changes to output from 02 script

--- from_hyak_transfer/2022-07-27_det_hist/02.5_join_det_hist.R
+++ from_hyak_transfer/2022-07-27_det_hist/02.5_join_det_hist.R
@@ -36,12 +36,67 @@ det_hist1_4 %>%
   bind_rows(., det_hist9_11) %>% 
   bind_rows(., det_hist12_14) -> complete_det_hist
 
+##### post-hoc fixes #####
+
+# Fix 1: Fish that are seen in the exit antennas at BO2, but then skip BO4, are actually not aborting, but just missed detections at BO4
+complete_det_hist %>% 
+  mutate(non_ascent = ifelse(end_ant_group %in% c("BO2-WEIR 52", "BO2-WEIR 51", "BO2-UMT Entrance"), NA, non_ascent)) -> complete_det_hist
+
+# Fix 2: Fish that are seen in the exit antennas at BO3, but then skip BO4, are actually not aborting, but just missed detections at BO4
+# Use the exit coils, same as Susannah
+complete_det_hist %>% 
+  mutate(non_ascent = ifelse(end_ant_group %in% c("BO3-WEIR 59", "BO3-WEIR 58"), NA, non_ascent)) -> complete_det_hist
+
+# Fix 3: Fish that are taking a very long time to pass through BO1
+complete_det_hist %>% 
+  mutate(to_remove = "no") -> complete_det_hist
+
+# this for loop doesn't work yet
+
+for (i in 1:(nrow(complete_det_hist)-1)){
+  if (complete_det_hist[i, 'tag_code'] == complete_det_hist[i+1, 'tag_code'] & 
+      complete_det_hist[i, 'event_site_name'] == "BO1 - Bonneville Bradford Is. Ladder" & 
+      complete_det_hist[i+1, 'event_site_name'] == "BO1 - Bonneville Bradford Is. Ladder" &
+      complete_det_hist[i, 'end_ant_group'] %in% c("A-BRANCH WEIR 50", "A-BRANCH WEIR 50",
+                           "B-BRANCH WEIR 50", "B-BRANCH WEIR 50") &
+      complete_det_hist[i+1, 'start_ant_group'] == "VERTICAL SLOT DETECTORS") {
+    
+    # Change non-ascent to not aborted
+    complete_det_hist[i, "non_ascent"] <- NA
+    
+    # Move the end information from the subsequent row to the current row
+    complete_det_hist[i, 'end_time'] <- complete_det_hist[i+1, 'end_time']
+    complete_det_hist[i, 'end_antenna_id'] <- complete_det_hist[i+1, 'end_antenna_id']
+    complete_det_hist[i, 'end_ant_group'] <- complete_det_hist[i+1, 'end_ant_group']
+    
+    # then, identify rows to remove - these are the rows following the current, from
+    # which we have pulled the necessary information
+    complete_det_hist[i+1, "to_remove"] <- "yes"
+    
+  }
+}
+
+# Now, remove those rows
+complete_det_hist %>% 
+  subset(to_remove == "no") -> complete_det_hist
+
+
 # Export this file
 
 write.csv(complete_det_hist, here::here("from_hyak_transfer", "2022-07-27_det_hist", "complete_det_hist.csv"))
 
 
-# There are some clearly weird things happening here
+# There are some clearly weird things happening here - these fixes are then incorporated above.
+
+# Look into BO1 fish
+complete_det_hist %>% 
+  group_by(tag_code) %>% 
+  filter(any(end_ant_group %in% c("A-BRANCH WEIR 50", "A-BRANCH WEIR 50",
+                                  "B-BRANCH WEIR 50", "B-BRANCH WEIR 50"))) -> BO1_branch_ends
+
+# so, majority of these fish are actually just hanging out in BO1 for a long time
+
+
 # Look into detections at BO2-BO3-BO4 that end in BO2 or BO3. Based on the specific weirs,
 # as well as where they were seen next, we can tell if these were really aborted ascension attempts or not
 
@@ -50,10 +105,9 @@ complete_det_hist %>%
   group_by(tag_code) %>% 
   filter(any(end_ant_group %in% c("BO3-WEIR 59", "BO3-WEIR 58"))) -> BO3_exit_ends
 
-# So what we're seeing here is that individuals either took a long time to get to BO4 (most of these fish), 
-# or just skip BO4 entirely
+# These fish are almost all not aborting, just are being missed in BO4
 
-# see how long it took them to get to BO3
+# some are ambiguous, if they're next sen in BO4
 BO3_exit_ends %>% 
   group_by(tag_code) %>% 
   filter(grepl("BO4",lead(end_ant_group)) & end_ant_group %in% c("BO3-WEIR 59", "BO3-WEIR 58") |
