907bbee32e51ab13e4c03998507d7b319ce234c4
Markus Min <markus.min@gmail.com>
2023-06-13 16:24:48 -0700

    exported yhat temps

--- R/23_temperature_MAR_model.R
+++ R/23_temperature_MAR_model.R
@@ -25,6 +25,17 @@ ICH_temp <- read.csv(here::here("covariate_data", "for_model", "temp", "ICH_wind
 WEL_temp <- read.csv(here::here("covariate_data", "for_model", "temp", "WEL_window_temp.csv")) %>% dplyr::select(date, tailrace_temp, forebay_temp)
 LGR_temp <- read.csv(here::here("covariate_data", "for_model", "temp", "LGR_window_temp.csv")) %>% dplyr::select(date, tailrace_temp, forebay_temp)
 
+# make the column data names more informative by including dam
+colnames(BON_temp) <- paste0("BON_", colnames(BON_temp))
+colnames(MCN_temp) <- paste0("MCN_", colnames(MCN_temp))
+colnames(PRA_temp) <- paste0("PRA_", colnames(PRA_temp))
+colnames(RIS_temp) <- paste0("RIS_", colnames(RIS_temp))
+colnames(RRE_temp) <- paste0("RRE_", colnames(RRE_temp))
+colnames(ICH_temp) <- paste0("ICH_", colnames(ICH_temp))
+colnames(WEL_temp) <- paste0("WEL_", colnames(WEL_temp))
+colnames(LGR_temp) <- paste0("LGR_", colnames(LGR_temp))
+
+
 # reformat data for model - drop date and transpose, bind all dams together
 t(BON_temp[,2:3]) %>% 
   rbind(., t(MCN_temp[,2:3])) %>% 
@@ -38,16 +49,112 @@ t(BON_temp[,2:3]) %>%
 dat = temp_for_MAR
 # fit the model
 
-# no bias term, no errors for observation process, no 
-R <- A <- U <- "zero"
-B <- Z <- "identity"
-Q <- "equalvarcov"
+
+# A are the offsets/biases for each dam - eight elements, each repeated twice
+# BON is the reference
+# a <- matrix(data = c("a_BON", "a_BON", "a_MCN", "a_MCN", "a_PRA", "a_PRA", "a_RIS", "a_RIS",
+#        "a_RRE", "a_RRE", "a_ICH", "a_ICH", "a_WEL", "a_WEL", "a_LGR", "a_LGR"),
+#        nrow = 16, ncol = 1)
+a <- matrix(data = c(0, 0, "a_MCN", "a_MCN", "a_PRA", "a_PRA", "a_RIS", "a_RIS",
+       "a_RRE", "a_RRE", "a_ICH", "a_ICH", "a_WEL", "a_WEL", "a_LGR", "a_LGR"),
+       nrow = 16, ncol = 1)
+A <- a
+
+# no bias term for Columbia river temp
+U <- "zero"
+
+# just one process and no interactions
+B <- matrix(1)
+
+# Z is sixteen observations of the same process
+Z <- matrix(1, 16, 1)
+
+# we need eight elements to correspond to the 
+# eight dams, and each element will be represented twice (once for forebay and once for tailrace)
+r <- matrix(data = as.list(rep(0, 256)), nrow = 16, ncol = 16)
+diag(r) <- c("r_BON", "r_BON", "r_MCN", "r_MCN", "r_PRA", "r_PRA", "r_RIS", "r_RIS",
+             "r_RRE", "r_RRE", "r_ICH", "r_ICH", "r_WEL", "r_WEL", "r_LGR", "r_LGR")
+R <- r
+Q <- matrix("q") # this matrix is a 1x1 matrix, because there's only one trend we're estimating
 model.list <- list(B = B, U = U, Q = Q, Z = Z, A = A, R = R)
 kem <- MARSS(dat, model = model.list)
 
+# plot MARSS model fit
+fit_df <- data.frame(date = seq(ymd('2005-06-01'),ymd('2022-12-31'), by = 'days'),
+                     fit = kem$states)
+
+# look at parameter estimates
+kem
+
+# some have considerably larger r (variance) values. Why?
+# is it because of the difference between forebay and tailrace temperatures?
+BON_temp[,2] - BON_temp[,3] -> BON_fore_tail_diff
+summary(BON_fore_tail_diff)
+MCN_temp[,2] - MCN_temp[,3] -> MCN_fore_tail_diff
+summary(MCN_fore_tail_diff)
+PRA_temp[,2] - PRA_temp[,3] -> PRA_fore_tail_diff
+summary(PRA_fore_tail_diff)
+RIS_temp[,2] - RIS_temp[,3] -> RIS_fore_tail_diff
+summary(RIS_fore_tail_diff)
+RRE_temp[,2] - RRE_temp[,3] -> RRE_fore_tail_diff
+summary(RRE_fore_tail_diff)
+ICH_temp[,2] - ICH_temp[,3] -> ICH_fore_tail_diff
+summary(ICH_fore_tail_diff)
+WEL_temp[,2] - WEL_temp[,3] -> WEL_fore_tail_diff
+summary(WEL_fore_tail_diff)
+LGR_temp[,2] - LGR_temp[,3] -> LGR_fore_tail_diff
+summary(LGR_fore_tail_diff)
+
+# plot Columbia River temperature
+dates <- seq(ymd('2005-06-01'),ymd('2022-12-31'), by = 'days')
+
+par(mfrow = c(1,1))
 
 
+plot(dates, kem$states, ylab = "Columbia River Temperature", 
+     xlab = "", type = "l")
+lines(dates, kem$states - 1.96 * kem$states.se, type = "l", lwd = 1, lty = 2, col = "red")
+lines(dates, kem$states + 1.96 * kem$states.se, type = "l", lwd = 1, lty = 2, col = "red")
+title("Columbia River Temperature")
 
+
+# extract the y hat values
+hat_yt <- MARSShatyt(kem)
+
+# plot the yhat for each dam
+# add points for actual data
+
+par(mfrow = c(4, 4))
+for (i in 1:16) {
+  plot(dates, kem$ytT[i, ], ylab = "Temperature", 
+       xlab = "", type = "l")
+  lines(dates, kem$ytT[i, ] - 1.96 * kem$ytT.se[i, 
+  ], type = "l", lwd = 1, lty = 2, col = "red")
+  lines(dates, kem$ytT[i, ] + 1.96 * kem$ytT.se[i, 
+  ], type = "l", lwd = 1, lty = 2, col = "red")
+  # points(dates, dat[i, ], cex = 0.2)
+  title(rownames(dat)[i])
+}
+
+# plot just the data
+par(mfrow = c(4, 4))
+for (i in 1:16) {
+  plot(dates, dat[i, ], cex = 0.4)
+  title(rownames(dat)[i])
+}
+
+# export the yhat values for tailrace temperatures for modeling
+temp_yhat <- data.frame(date = dates,
+                        BON = kem$ytT[1,],
+                        MCN = kem$ytT[3,],
+                        PRA = kem$ytT[5,],
+                        RIS = kem$ytT[7,],
+                        RRE = kem$ytT[9,],
+                        ICH = kem$ytT[11,],
+                        WEL = kem$ytT[13,],
+                        LGR = kem$ytT[15,])
+
+write.csv(temp_yhat, here::here("covariate_data", "for_model", "temp", "temp_yhat.csv"), row.names = FALSE)
 
 
 
